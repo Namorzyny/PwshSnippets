ae858b52725f3702f9a46d5a7467a71729b9d357
Namorzyny <namorzyny@gmail.com>
2023-11-22 16:51:47 +0800

    Use local module temporarily

--- 20-auto_install.ps1
+++ 20-auto_install.ps1
@@ -1,19 +1,19 @@
 Invoke-Command {
 	$ProfileDirectory = $PROFILE.Substring(0, $PROFILE.LastIndexOf('\'))
 	$ModuleCheckList = @(
+		'PSBashCompletions',
 		'PSFzf',
 		'WSLTabCompletion',
 		'posh-git',
 		'scoop-completion',
 		'npm-completion',
-		'yarn-completion',
-		'CondaTabExpansion'
+		'yarn-completion'
 	)
 
 	$ModuleCheckList | ForEach-Object {
 		if ($null -eq (Get-Item -Path "$ProfileDirectory\Modules\$_" -ErrorAction Ignore)) {
 			Write-Warning "Missing module: $_, Installing..."
-			Install-Module -Name $_ -Scope CurrentUser -AllowClobber
+			Install-Module -Name $_ -Scope CurrentUser
 		}
 	}
 }
